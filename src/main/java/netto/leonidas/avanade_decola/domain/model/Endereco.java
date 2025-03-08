6c2819132f488ab749714186f27dc695c0766c7c
ldcnetto <ldcn@cin.ufpe.br>
2025-03-08 13:44:54 -0300

    feat: classe endereço

--- src/main/java/netto/leonidas/avanade_decola/domain/model/Endereco.java
+++ src/main/java/netto/leonidas/avanade_decola/domain/model/Endereco.java
@@ -1,4 +1,54 @@
 package netto.leonidas.avanade_decola.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity()
 public class Endereco {
+    @Id
+    private int cep;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String tipo;
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
