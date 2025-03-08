8a80f16d96c8e86654b3fcc0ffe1beb246c3fbfb
ldcnetto <ldcn@cin.ufpe.br>
2025-03-08 14:27:34 -0300

    feat: criando classe da relação ProdutoFornecedor e adicionado getters, setters e config do JPA

---
+++ src/main/java/netto/leonidas/avanade_decola/model/ProdutoFornecedor.java
@@ -0,0 +1,71 @@
+package netto.leonidas.avanade_decola.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Produtos_fornecedores")
+public class ProdutoFornecedor {
+    @EmbeddedId
+    private ProdutoFornecedorId id;
+
+    @ManyToOne
+    @MapsId("idProduto")
+    @JoinColumn(name = "id_produto", nullable = false)
+    private Produto produto;
+
+    @ManyToOne
+    @MapsId("idFornecedor")
+    @JoinColumn(name = "id_fornecedor", nullable = false)
+    private Fornecedor fornecedor;
+
+    public ProdutoFornecedorId getId() {
+        return id;
+    }
+
+    public void setId(ProdutoFornecedorId id) {
+        this.id = id;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+}
+
+@Embeddable
+class ProdutoFornecedorId implements Serializable {
+    @Column(name = "id_produto")
+    private int idProduto;
+
+    @Column(name = "id_fornecedor")
+    private int idFornecedor;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+}
