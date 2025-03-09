0e2cfc7f5a28ef3557b47b65443d62b4fda1a7be
ldcnetto <ldcn@cin.ufpe.br>
2025-03-09 01:46:22 -0300

    bugfix: ajuste CORS

--- src/main/java/netto/leonidas/avanade_decola/AvanadeDecolaApplication.java
+++ src/main/java/netto/leonidas/avanade_decola/AvanadeDecolaApplication.java
@@ -1,12 +1,14 @@
 package netto.leonidas.avanade_decola;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class AvanadeDecolaApplication {
-
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
