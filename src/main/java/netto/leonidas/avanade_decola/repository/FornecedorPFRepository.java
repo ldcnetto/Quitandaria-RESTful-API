7b23e474451e11c0bf945c70dab6dfb08fc28571
ldcnetto <ldcn@cin.ufpe.br>
2025-03-09 01:08:31 -0300

    refactor: tornando opcional

--- src/main/java/netto/leonidas/avanade_decola/repository/FornecedorPFRepository.java
+++ src/main/java/netto/leonidas/avanade_decola/repository/FornecedorPFRepository.java
@@ -3,8 +3,9 @@ package netto.leonidas.avanade_decola.repository;
 import netto.leonidas.avanade_decola.model.FornecedorPF;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface FornecedorPFRepository extends JpaRepository<FornecedorPF, String> {
-    FornecedorPF findByCpf(String cpf);
+    Optional<FornecedorPF> findByCpf(String cpf);
 }
--- src/main/java/netto/leonidas/avanade_decola/repository/FornecedorRepository.java
+++ src/main/java/netto/leonidas/avanade_decola/repository/FornecedorRepository.java
@@ -4,9 +4,10 @@ import netto.leonidas.avanade_decola.model.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
     List<Fornecedor> findByRazaoSocialContaining(String razaoSocial);
-    Fornecedor findByEmail(String email);
+    Optional<Fornecedor> findByEmail(String email);
 }
