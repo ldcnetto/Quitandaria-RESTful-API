db714b1e5d8ada21d9e0282b1aabbc46e61265a7
ldcnetto <ldcn@cin.ufpe.br>
2025-03-09 01:17:26 -0300

    feat: classe error response

---
+++ src/main/java/netto/leonidas/avanade_decola/controller/exception/ErrorResponse.java
@@ -0,0 +1,21 @@
+package netto.leonidas.avanade_decola.controller.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+    private HttpStatus status;
+    private String message;
+
+    public ErrorResponse(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
